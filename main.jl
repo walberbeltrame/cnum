29e0bbcf1d36fbb2d867fbb0c017dfef87d7aec0
Walber Antonio Ramos Beltrame <walber.beltrame@gmail.com>
2022-08-17 17:47:06 +0000

    Segunda aula

--- main.jl
+++ main.jl
@@ -1,18 +1,1 @@
-v = 1
-
-println(v)
-
-f(x) = x^2 + x + 1
-
-z = f(2)
-
-println(z)
-
-function fx(x, a, b, c)
-  y(n) = a*n^2 + b*n + c
-  return y(x)
-end
-
-z = fx(1,2,3,4) + 1
-
-println(z)
+include("2/main.jl")
