ad7e4f75d7408a21f439e670a7eef05a8a12d884
walber.beltrame <walber.beltrame@gmail.com>
2025-08-21 18:47:40 +0000

    Exercício 4

--- 4/main.py
+++ 4/main.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 
 from algoritmos import bissecao
 
-# Definição da função
+# Função da Atividade1
 def f1(x):
     return x**3 - x - 2
 
-def plot(f, number=1):
+# Função da Atividade2
+def f2(x):
+    return x**(1/2) - np.cos(x)
+
+def plot(f, xi, xf, d=0.1, num_img=1):
     # Intervalo para plotar
-    x_vals = np.arange(-2, 3, 0.1)
+    x_vals = np.arange(xi, xf, d)
     y_vals = f(x_vals)
 
     plt.axhline(0, color="black", linewidth=1)  # eixo x
@@ -19,14 +23,19 @@ def plot(f, number=1):
     plt.title("Visualização da função f(x)")
 
     # Salvar gráfico como imagem
-    plt.savefig(f"4/bissecao_{number}.png", dpi=120, bbox_inches="tight")
+    plt.savefig(f"4/bissecao_{num_img}.png", dpi=120, bbox_inches="tight")
     plt.close()
 
 def main():
     # Atividade 1
     print("-- Atividade 1 --")
-    plot(f1, 1)
+    plot(f1, -2, -3, num_img=1)
     r, i = bissecao(f1, 1, 2, 1e-15)
+    print(f"raiz = {r} , i = {i}")
+    # Atividade 2
+    print("-- Atividade 2 --")
+    plot(f2, 0, 1, num_img=2)
+    r, i = bissecao(f2, 0, 1, 1e-15)
     print(f"raiz = {r} , i = {i}")
 
 if __name__ == "__main__":
